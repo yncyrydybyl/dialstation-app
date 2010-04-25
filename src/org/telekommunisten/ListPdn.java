250ca8bc674147b40eafd6e78f858d5a8606d4ff
Dirk Jaeckel <github@dirk.jaeckel.name>
2010-04-26 00:04:40 +0200

    in der Liste pdns steht jetzt nur noch jeweils die psdn. bei auswahl wird gewaehlt

--- src/org/telekommunisten/ListPdn.java
+++ src/org/telekommunisten/ListPdn.java
@@ -17,83 +17,92 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 public class ListPdn extends ListActivity {
-	    
-	    /** Called when the activity is first created. */
-	
-		@Override
-		public void onListItemClick(ListView l, View v, int pos,long id)
-		{
-			String pdntext = (String) l.getItemAtPosition(pos);
-			Log.d("dialstation",pdntext);
-		}
-	    @Override
-	    public void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	        
-	        final Cursor cursor = getContentResolver().query(Uri.parse("content://com.dialstation"), new String[] {"pdns"},null,null,null);
-	        
-	        setListAdapter(new BaseAdapter() {
-				
-				@Override
-				public View getView(int position, View convertView, ViewGroup parent) {
-					if (convertView == null) {
-						convertView = new TextView(ListPdn.this);						
-					} 
-					
-					cursor.moveToPosition(position);
-					((TextView) convertView).setText(cursor.getString(1)+"\n"+cursor.getString(cursor.getColumnIndex("destination")));
-					return convertView;
-				}
-				
-				@Override
-				public long getItemId(int position) {
-					cursor.moveToPosition(position);
-					return Long.valueOf(cursor.getString(cursor.getColumnIndex("id")));
 
-					// TODO Auto-generated method stub
-				}
-				
-				@Override
-				public Object getItem(int position) {
-					// TODO Auto-generated method stub
-					cursor.moveToPosition(position);
-					return cursor.getString(1)+"\n"+cursor.getString(cursor.getColumnIndex("destination"));
-				}
-				
-				@Override
-				public int getCount() {
-					// TODO Auto-generated method stub
-					Log.d("ListPdn",cursor.getCount()+"<- cursor.length");
-					return cursor.getCount();
-				}
-			});
-	    }
+    /**
+     * Called when the activity is first created.
+     */
 
-	    @Override
-	    public boolean onCreateOptionsMenu(Menu menu) {
-	        MenuInflater inflater = getMenuInflater();
-	        inflater.inflate(R.menu.main, menu);
-	        return super.onCreateOptionsMenu(menu);
-	    }
+    @Override
+    public void onListItemClick(ListView l, View v, int pos, long id) {
+        String pdntext = (String) l.getItemAtPosition(pos);
+        Log.d("dialstation", pdntext);
 
-	    @Override
-	    public boolean onOptionsItemSelected(MenuItem item) {
-	        switch (item.getItemId()) {
+//        startActivity(new Intent(Intent.ACTION_DIAL,  Uri.parse("tel:+491792900944")));
+//        startActivity(new Intent(Intent.ACTION_DIAL,  Uri.parse("tel:+" + pdntext )));
+        startActivity(new Intent(Intent.ACTION_CALL,  Uri.parse("tel:+" + pdntext )));
 
-	        case R.id.search:
-	            onSearchRequested();
-	            break;
+    }
 
-	        case R.id.settings:
-	            startActivity(new Intent(this, SettingsActivity.class));
-	            break;
-	        case R.id.feedback:
-	            LogCollector.feedback(this, "flo@andlabs.de", "blah blah blah");
-	            break;
-	        }
-	        return super.onOptionsItemSelected(item);
-	    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-	}
+        final Cursor cursor = getContentResolver().query(Uri.parse("content://com.dialstation"), new String[]{"pdns"}, null, null, null);
+
+        setListAdapter(new BaseAdapter() {
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                if (convertView == null) {
+                    convertView = new TextView(ListPdn.this);
+                }
+
+                cursor.moveToPosition(position);
+//					((TextView) convertView).setText(cursor.getString(1)+"\n"+cursor.getString(cursor.getColumnIndex("destination")));
+                ((TextView) convertView).setText(cursor.getString(1));
+                return convertView;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                cursor.moveToPosition(position);
+                return Long.valueOf(cursor.getString(cursor.getColumnIndex("id")));
+
+                // TODO Auto-generated method stub
+            }
+
+            @Override
+            public Object getItem(int position) {
+                // TODO Auto-generated method stub
+                cursor.moveToPosition(position);
+//                return cursor.getString(1) + "\n" + cursor.getString(cursor.getColumnIndex("destination"));
+                return cursor.getString(1);
+            }
+
+            @Override
+            public int getCount() {
+                // TODO Auto-generated method stub
+                Log.d("ListPdn", cursor.getCount() + "<- cursor.length");
+                return cursor.getCount();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+
+            case R.id.search:
+                onSearchRequested();
+                break;
+
+            case R.id.settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                break;
+            case R.id.feedback:
+                LogCollector.feedback(this, "flo@andlabs.de", "blah blah blah");
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
 	
 	
